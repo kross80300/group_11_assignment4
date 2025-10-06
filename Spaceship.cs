caa610f83dc06e1276570bb15b47fa3b3b71bf5c
Spencerars <spencerar264@gmail.com>
2025-10-05 22:15:45 -0500

    Add files via upload
    I cannot get this to run and I have no idea why. I can't test the thruster texture offset because of this.

---
+++ Spaceship.cs
@@ -0,0 +1,67 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace sra2745_assignment4;
+
+public class Spaceship
+{
+    public Vector2 Origin { get; set; } = Vector2.Zero;
+    public Vector2 Speed { get; set; } = new Vector2(1, 0);
+    public Texture2D Texture { get; set; }
+    public Texture2D ThrustTexture { get; set; }
+
+    public float Scale { get; set; } = 0.7f;
+    public Spot(Texture2D texture)
+    {
+        Texture = texture;
+    }
+
+    public Spaceship(Texture2D texture, Texture2D thrustTexture, Vector2 position, Vector2 speed, float scale)
+    {
+        Texture = texture;
+        ThrustTexture = thrustTexture;
+        Origin = position;
+        Speed = speed;
+        Scale = scale;
+        Direction = speed;
+    }
+
+    // Move and wrap when reaching the edge
+    public void Move(int frameWidth, int frameHeight, float scale)
+    {
+        var newOrigin = Origin + Speed;
+        if (newOrigin.X > frameWidth)
+        {
+            newOrigin.X = 0 - Texture.Width * scale;
+        }
+        if (newOrigin.Y > frameHeight)
+        {
+            newOrigin.Y = 0 - Texture.Height * scale;
+        }
+        Origin = newOrigin;
+    }
+
+    public void display(SpriteBatch spriteBatch, float scale)
+    {
+        spriteBatch.Draw(Texture, Origin, null, Color.White, 0f, Vector2.Zero, Scale, SpriteEffects.None, 0f);
+        if (Speed.X > 0 || Speed.Y > 0)
+        {
+            spriteBatch.Draw(ThrustTexture, Origin + new Vector2(-28 * scale, 5 * scale), null, thrustEffect(scale, Speed), 0f, Vector2.Zero, Scale, SpriteEffects.None, 0f);
+        }
+    }
+
+    public Color thrustEffect(float scale, Vector2 speed)
+    {
+        int baseColor = Math.Clamp((int)(speed.X * 30 + speed.Y * 30), 0, 255);
+        byte r = (byte)baseColor;
+        byte g = (byte)(baseColor * 0.9f);
+        byte b = (byte)(baseColor * 0.8f);
+        return new Color(r, g, b);
+    }
+
+
+
+
+
+}
